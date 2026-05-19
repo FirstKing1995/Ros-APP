f79f9ea91c5c2baca1a0d519e8c56b6d4113eaba
FirstKing1995 <dhionematheus@gmail.com>
2026-05-19 20:51:03 -0300

    Update admin-afiliados.js

--- admin-afiliados.js
+++ admin-afiliados.js
@@ -1,7 +1,7 @@
 // ==========================================
 // ROSÊ APP — admin-afiliados.js
 // ==========================================
-const API_URL = 'SUA_URL_DO_GOOGLE_APPS_SCRIPT_AQUI';
+const API_URL = 'https://script.google.com/macros/s/AKfycbzzYjT12l48onOPS3yYCA-CwsUsnTBmNWQEHs6YpQSLG7B6tkxvB8d7EPbnJhNa1noS1A/exec';
 
 window.alert = function(mensagem) {
     Swal.fire({ text: mensagem, background: '#1C1C1E', color: '#FFFFFF', confirmButtonColor: '#B76E79', customClass: { popup: 'borda-arredondada' }});
